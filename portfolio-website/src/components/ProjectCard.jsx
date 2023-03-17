755e04ac7a0e518dba58258118c5eef66aa9aec5
+ <+>
2023-03-17 15:52:15 -0600

    general project cards layout

--- portfolio-website/src/components/ProjectCard.jsx
+++ portfolio-website/src/components/ProjectCard.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 import github from "../assets/github-mark-white.png";
+import "../index.css";
 
 const ProjectCard = ({ project }) => {
   const { title, description, image, repo, tags } = project;
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full" src={image} alt="Sunset in the mountains" />
-      <div className="px-6 py-4 bg-white">
-        <div className="font-bold text-xl mb-2 text-black">{title}</div>
-        <p className="text-[#0097b2] text-base">{description}</p>
+    <div className="max-w-sm rounded-xl overflow-hidden shadow-lg m-2">
+      <img className="projectimage" src={image} alt={title} />
+      <div className="px-6 py-4 bg-gray-200">
+        <div className="font-bold text-xl text-black mb-2">{title}</div>
+        <p className="text-gray-700 text-base">{description}</p>
       </div>
       <div className="px-6 bg-[#0097b2]">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #{tags[0]}
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          {tags[0]}
         </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #{tags[1]}
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          {tags[1]}
         </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #{tags[2]}
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          {tags[2]}
         </span>
-        <a href={repo} className="inline-block px-3 mt-4">
-          <img src={github} alt="github" className="w-8 h-8" />
-        </a>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          <a href="repo">
+            <img src={github} alt="github" className="w-6 h-6" />{" "}
+          </a>
+        </span>
       </div>
     </div>
   );
--- portfolio-website/src/components/Projects.jsx
+++ portfolio-website/src/components/Projects.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
+import asyncedu from "../assets/asyncedu.png";
+import allinai from "../assets/allinai.png";
 
 const projects = [
   {
     title: "AsyncEdu",
-    description: "This is a description",
-    image: "../assets/navbarcode.png",
+    description:
+      "AsyncEdu is a chat-based application that can be used for remote or asynchronous instructional support, communication, and collaboration between teachers and students.",
+    image: asyncedu,
     repo: "https://github.com/andrewjulian/phase-5-project",
     tags: ["React", "Rails", "PostGreSQL"],
+  },
+  {
+    title: "All In AI Website",
+    description:
+      "This website was created for All in AI, a non-profit organization that aims to increase diversity in the field of AI and provide access to AI education for underrepresented groups.",
+    image: allinai,
+    repo: "https://github.com/andrewjulian/all-in-ai-website-v3",
+    tags: ["HTML5", "CSS3", "Bootstrap"],
   },
 ];
 
@@ -16,7 +27,11 @@ const projectCards = projects.map((project) => {
 });
 
 const Projects = () => {
-  return <div className="bg-red">{projectCards}</div>;
+  return (
+    <div className="flex flex-col items-center md:flex-row p-10">
+      {projectCards}
+    </div>
+  );
 };
 
 export default Projects;
