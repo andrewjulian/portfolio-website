2e6b560599e9f18fbb65ea9ab0f81a29cdfbc305
+ <+>
2023-03-21 16:03:10 -0600

    fixed hover on projects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,28 +6,30 @@ import Zoom from "react-reveal/Zoom";
 const ProjectCard = ({ project }) => {
   const { title, description, image, repo, tags } = project;
   return (
-    <Zoom>
-      <div className="max-w-sm rounded-xl overflow-hidden shadow-lg m-2 transition ease-in-out delay-100 hover:scale-110 duration-300">
-        <img className="projectimage" src={image} alt={title} />
-        <div className="px-6 py-4 bg-gray-200">
-          <div className="font-bold text-xl text-black mb-2">{title}</div>
-          <p className="text-gray-700 text-base">{description}</p>
+    <div className="max-w-sm rounded-xl overflow-hidden shadow-lg m-2 hover:scale-110 duration-300">
+      <Zoom>
+        <div>
+          <img className="projectimage" src={image} alt={title} />
+          <div className="px-6 py-4 bg-gray-200">
+            <div className="font-bold text-xl text-black mb-2">{title}</div>
+            <p className="text-gray-700 text-base">{description}</p>
+          </div>
+          <div className=" flex px-2 sm:px-6 bg-[#0097b2] justify-center items-center">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 my-2 ">
+              {tags[0]}
+            </span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 my-2 ">
+              {tags[1]}
+            </span>
+            <span className="flex items-end justify-end px-3 py-1 text-sm font-semibold text-gray-700  my-2">
+              <a href={repo} target="blank">
+                <img src={github} alt="github" className="w-8 sm:h-8" />
+              </a>
+            </span>
+          </div>
         </div>
-        <div className=" flex px-2 sm:px-6 bg-[#0097b2] justify-center items-center">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 my-2 ">
-            {tags[0]}
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 my-2 ">
-            {tags[1]}
-          </span>
-          <span className="flex items-end justify-end px-3 py-1 text-sm font-semibold text-gray-700  my-2">
-            <a href={repo} target="blank">
-              <img src={github} alt="github" className="w-8 sm:h-8" />
-            </a>
-          </span>
-        </div>
-      </div>
-    </Zoom>
+      </Zoom>
+    </div>
   );
 };
 
